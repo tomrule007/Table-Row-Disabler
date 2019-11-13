0fd7bf25e2810ca2fd7983dbc2f1b368c17c6cad
Thomas Herzog <Tomrule007@gmail.com>
2019-11-13 11:43:33 -0800

    Add checklist to rows available on load
    This is just a prove of concept version, doesnt work with dynamic content

--- tableRowLocker.js
+++ tableRowLocker.js
@@ -1,40 +1,45 @@
 (function() {
   console.log('table-row-locker: tableRowLocker Injected!');
+  var tables = document.getElementsByTagName('table');
+  console.log('tables.length: ', tables.length);
 
-  // // Attach focusin/focusout listeners to the document;
-  // document.addEventListener('focusin', onFocusIn, true);
-  // document.addEventListener('focusout', onFocusOut, true);
+  var tableRows = document.getElementsByTagName('tr');
+  console.log('tablerows.length', tableRows.length);
 
-  // function onFocusIn({ target }) {
-  //   setStyle(
-  //     getRowNode(target),
-  //     'font-weight: bold; background-color: yellow; outline: thin solid'
-  //   );
-  // }
+  [...tableRows].forEach(row => addCheckBox(row));
 
-  // function onFocusOut({ target }) {
-  //   setStyle(getRowNode(target), '');
-  // }
+  function addCheckBox(row) {
+    var td = document.createElement('td');
+    td.innerHTML = '<input type="checkbox" class="rowDisablerCheckBox">';
 
-  // function getRowNode(el) {
-  //   const MAX_DEPTH = 5;
-  //   let rowNode;
-  //   let curEl = el;
-  //   for (let i = 0; i < MAX_DEPTH; i++) {
-  //     if (curEl && curEl.tagName === 'TR') {
-  //       rowNode = curEl;
-  //       break;
-  //     }
-  //     curEl && (curEl = curEl.parentNode);
-  //     if (MAX_DEPTH === i + 1)
-  //       console.log(
-  //         'highlight-active-row: Unable to find Row Element. Increase MAX_DEPTH'
-  //       );
-  //   }
-  //   return rowNode;
-  // }
+    row.prepend(td);
+    console.log(row);
+  }
 
-  // function setStyle(el, style) {
-  //   el && (el.style.cssText = style);
-  // }
+  function disableRow({ target }) {
+    // Do nothing if the checkbox is not our own.
+    if (target.dataset.rowDisabler !== 'rowDisabler') return;
+    setStyle;
+  }
+
+  function getRowNode(el) {
+    const MAX_DEPTH = 5;
+    let rowNode;
+    let curEl = el;
+    for (let i = 0; i < MAX_DEPTH; i++) {
+      if (curEl && curEl.tagName === 'TR') {
+        rowNode = curEl;
+        break;
+      }
+      curEl && (curEl = curEl.parentNode);
+      if (MAX_DEPTH === i + 1)
+        console.log(
+          'table-row-locker: Unable to find Row Element. Increase MAX_DEPTH'
+        );
+    }
+    return rowNode;
+  }
+  function setStyle(el, style) {
+    el && (el.style.cssText = style);
+  }
 })();
