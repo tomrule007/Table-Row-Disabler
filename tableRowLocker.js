eb1016d449e07368b83397e778d386f3223a7eb8
Thomas Herzog <Tomrule007@gmail.com>
2019-11-13 12:00:02 -0800

    Add Example mutation Observer script

--- tableRowLocker.js
+++ tableRowLocker.js
@@ -8,6 +8,33 @@
 
   [...tableRows].forEach(row => addCheckBox(row));
 
+  // Mutation Observer Example
+  // Source: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+  // Select the node that will be observed for mutations
+  const targetNode = document.getElementById('unique-table-id');
+
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  const callback = function(mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        console.log('A child node has been added or removed.');
+      } else if (mutation.type === 'attributes') {
+        console.log(
+          'The ' + mutation.attributeName + ' attribute was modified.'
+        );
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+
   function addCheckBox(row) {
     var td = document.createElement('td');
     td.innerHTML = '<input type="checkbox" class="rowDisablerCheckBox">';
