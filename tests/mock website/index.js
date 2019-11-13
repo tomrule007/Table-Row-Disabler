392ba4b7c429fd1f3223ec8cee711c4b31ee91f1
Thomas Herzog <Tomrule007@gmail.com>
2019-11-13 11:40:53 -0800

    Test site saves last row count
    This allows page refreshing for testing rows on load

--- tests/mock website/index.js
+++ tests/mock website/index.js
@@ -1,41 +1,56 @@
+// Global Variables
+// Reads stored rowCount (null is coerced into 0)
+var rowCount = Number(localStorage.getItem('rowCount'));
+var rowInterval;
+
 // Event Listeners
-document.getElementById('addRow').addEventListener('click', addRow);
+document
+  .getElementById('addRow')
+  .addEventListener('click', addRowAndUpdateCount);
 document.getElementById('addInterval').addEventListener('click', addInterval);
 
 document.getElementById('stopInterval').addEventListener('click', stopInterval);
-document.getElementById('clearTable').addEventListener('click', clearTable);
+document
+  .getElementById('clearTable')
+  .addEventListener('click', clearTableAndResetCount);
 
 // HTML Elements
 const tbody = document.getElementById('tbody');
 
 const intervalTime = document.getElementById('intervalTime');
 
-// Global Variables
-var rowCounter = 0;
-var rowInterval;
-
+// Initialize previous set rows.
+for (let i = 0; i < rowCount; i++) {
+  addRow(i);
+}
 // Functions
+function addRowAndUpdateCount() {
+  console.log('add row and update count');
+  addRow(rowCount++);
+  localStorage.setItem('rowCount', rowCount);
+}
 
-function clearTable(ev) {
-  rowCounter = 0;
+function clearTableAndResetCount() {
+  rowCount = 0;
+  localStorage.setItem('rowCount', 0);
   tbody.innerHTML = '';
 }
 
-function addRow(ev) {
+function addRow(label) {
   console.log('addRow!');
   var row = document.createElement('tr');
-  row.innerHTML = `<td>${rowCounter++}</td><td> Blah Blah Blah </td> <td><input type="text"></td>`;
+  row.innerHTML = `<td>${label}</td><td> Blah Blah Blah </td> <td><input type="text"></td>`;
 
   tbody.appendChild(row);
 }
-function stopInterval(ev) {
+function stopInterval() {
   clearInterval(rowInterval);
 }
-function addInterval(event) {
+function addInterval() {
   console.log(+intervalTime.value);
   // check and clear old interval first
   if (rowInterval) clearInterval(rowInterval);
   rowInterval = setInterval(() => {
-    addRow();
+    addRowAndUpdateCount();
   }, +intervalTime.value);
 }
