1b1162dc542dabbf21a58aafcc70c64550cee7d2
Thomas Herzog <Tomrule007@gmail.com>
2019-11-20 08:16:26 -0800

    Refactor: removed content injector script.
    No reason to inject script into actual website.
    It functions perfectly in the isolated content_script sandbox.

--- contentScript.js
+++
@@ -1,7 +0,0 @@
-console.log('table-row-locker: Injecting tableRowLocker...');
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('tableRowLocker.js');
-s.onload = function() {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
