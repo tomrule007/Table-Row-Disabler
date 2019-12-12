f864e9e84c5a4598a7d336f2eee4477ffd8f2d10
Thomas Herzog <Tomrule007@gmail.com>
2019-12-11 21:03:14 -0800

    Refactored disableExtension / enableExtension.
    Converted both functions into a single function with an setEnable param.

--- src/background.js
+++ src/background.js
@@ -1,87 +1,64 @@
+import { version } from '../package.json';
+
 console.log('table-row-locker: background script');
 
 // utility functions
 const urlToDomain = url => url.match(/^.*\/\/.+?\//)[0];
 
-const disableExtension = tab => {
-  console.log('Setting Disabled Browser Action State');
+chrome.contextMenus.create({
+  id: 'enabledCheckbox',
+  contexts: ['browser_action'],
+  title: `Enable`,
+  type: 'checkbox',
+  checked: false
+});
+const setBrowserActionView = (setEnabled, tab) => {
   const { id: tabId } = tab;
 
-  // Update Context Menu
   chrome.contextMenus.update('enabledCheckbox', {
     title: `Enable on: ${urlToDomain(tab.url)}`,
-    checked: false
+    checked: setEnabled
   });
 
-  // Enable left click browser action
-  chrome.browserAction.enable(tabId);
+  const {
+    enable,
+    disable,
+    setTitle,
+    setIcon,
+    setBadgeText,
+    setBadgeBackgroundColor
+  } = chrome.browserAction;
 
-  // Update Title
-  chrome.browserAction.setTitle({
+  if (setEnabled) {
+    disable(tabId);
+  } else {
+    enable(tabId);
+  }
+
+  setTitle({
     tabId,
-    title: `Table-Row-Locker v0.1.0  
-Click to Enable!`
+    title: `Table-Row-Locker v${version} ${
+      setEnabled ? '' : '\nClick to Enable!'
+    }`
   });
 
-  // Update Icon
-  chrome.browserAction.setIcon({
+  setIcon({
     tabId,
-    path: 'img/tableRowLockerDisabledIcon16.png'
+    path: setEnabled
+      ? 'img/tableRowLockerIcon16.png'
+      : 'img/tableRowLockerDisabledIcon16.png'
   });
 
   // Set Badge text & color
-  chrome.browserAction.setBadgeText({ text: '!', tabId });
-  chrome.browserAction.setBadgeBackgroundColor({ color: '#F00', tabId });
+  setBadgeText({ text: setEnabled ? '' : '!', tabId });
+  if (!setEnabled) setBadgeBackgroundColor({ color: '#F00', tabId });
 };
 
-const enableExtension = tab => {
-  console.log('Setting Enabled Browser Action State');
-  const { id: tabId } = tab;
-
-  // Update Context Menu
-  chrome.contextMenus.update('enabledCheckbox', {
-    title: `Enable on: ${urlToDomain(tab.url)}`,
-    checked: true
-  });
-
-  // Disable left click browser action
-  chrome.browserAction.disable(tabId);
-
-  // Update Title
-  chrome.browserAction.setTitle({
-    tabId,
-    title: `Table-Row-Locker v0.1.0`
-  });
-
-  // Update Icon
-  chrome.browserAction.setIcon({
-    tabId,
-    path: 'img/tableRowLockerIcon16.png'
-  });
-  chrome.browserAction.setBadgeText({ text: '', tabId });
-};
-
-chrome.contextMenus.create(
-  {
-    id: 'enabledCheckbox',
-    contexts: ['browser_action'],
-    title: `Enable`,
-    type: 'checkbox',
-    checked: false
-  },
-  () => {
-    console.log(
-      chrome.runtime.lastError
-        ? `Error: ${chrome.runtime.lastError}`
-        : `'Enable' contextMenu successfully created`
-    );
-  }
-);
 chrome.contextMenus.onClicked.addListener(({ checked }, tab) => {
   if (checked) {
-    enableExtension(tab);
+    setBrowserActionView(true, tab);
   } else {
-    disableExtension(tab);
+    setBrowserActionView(false, tab);
   }
 });
 const sendActivateMsg = tab =>
@@ -90,7 +67,7 @@ const sendActivateMsg = tab =>
     storageKey: urlToDomain(tab.url)
   });
 chrome.browserAction.onClicked.addListener(tab => {
-  enableExtension(tab);
+  setBrowserActionView(true, tab);
   sendActivateMsg(tab);
 });
 // Setup message listener to communicate with content scripts.
@@ -104,11 +81,11 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
       chrome.storage.sync.get([storageKey], results => {
         if (results[storageKey]) {
           // Storage found! send activation message with storageKey
-          enableExtension(tab);
+          setBrowserActionView(true, tab);
           sendActivateMsg(tab);
         } else {
           // Storage not found disable and wait for browserAction click
-          disableExtension(tab);
+          setBrowserActionView(false, tab);
         }
       });
 
